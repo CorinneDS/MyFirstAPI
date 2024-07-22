9ee3d76d79d0407a44d2d1a48cba75b400c4f431
Corinne <cdumairir@hotmail.com>
2024-07-23 00:13:01 +0800

    ajout du test pytest de l'API

---
+++ test_api.py
@@ -0,0 +1,30 @@
+import pytest
+from flask import Flask
+import pandas as pd
+import random
+from api import app  # Remplacez 'your_flask_app' par 'api' ou le nom du module où se trouve votre application
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def random_test_data():
+    # Lire le fichier CSV
+    df = pd.read_csv('X_test50_woIndex.csv')
+    # Sélectionner une ligne au hasard
+    random_row = df.sample(n=1).to_dict(orient='records')[0]
+    return random_row
+
+def test_prediction_real_model(client, random_test_data):
+    # Envoyer une requête POST à la route /prediction avec les données de test aléatoires
+    response = client.post('/prediction', json=random_test_data)
+
+    # Vérifier que la réponse a le statut 200
+    assert response.status_code == 200
+
+    # Vérifier le contenu de la réponse
+    json_data = response.get_json()
+    assert 'prediction' in json_data
+    # Effectuer des vérifications sur json_data en fonction des prédictions attendues
